ce223fa1ee674aa292e728870316fdc33494596b
Aliaksandr Halaveika <aliaksandr_halaveika@epam.com>
2022-02-07 06:39:40 +0300

    feat: add react app environment and itils

---
+++ src/app.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+export default function App(): JSX.Element {
+  return <Layout className="container">Hello world</Layout>;
+}
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import App from './app';
+import './styles.scss';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
